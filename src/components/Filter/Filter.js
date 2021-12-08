27ade41d94099b5b9d9b36bad4588ae1265105b7
asimcanuz <asimcanuz@gmail.com>
2021-12-09 00:15:32 +0300

    Filtreleme islemi

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -10,7 +10,28 @@ const Filter = ({
   setGender,
   setSpecies,
 }) => {
-  return <div></div>;
+  const clear = () => {
+    setStatus("");
+    setSpecies("");
+    setGender("");
+    setPageNumber("");
+    window.location.reload(false);
+  };
+  return (
+    <div className="col-lg-3 col-12 mb-5">
+      <div className="text-center fw-bold fs-4 mb-2">Filters</div>
+      <div
+        style={{ cursor: "pointer" }}
+        onClick={clear}
+        className="text-primary text-decoration-underline text-center mb-3"
+      >
+        Clear Filters
+      </div>
+      <div className="accordion" id="accordionExample">
+        <Status setPageNumber={setPageNumber} setStatus={setStatus} />
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
--- src/components/Filter/category/Status.js
+++ src/components/Filter/category/Status.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Filterbtn from "../FilterBTN";
 
-const Status = () => {
-  return <div></div>;
+const Status = ({ setStatus, setPageNumber }) => {
+  // TODO: dizi tanımlamadan api'den status'ler çekilecek
+  let status = ["Alive", "Dead", "Unkown"];
+
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id="headingOne">
+        <button
+          className="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseOne"
+          aria-expanded="true"
+          aria-controls="collapseOne"
+        >
+          Status
+        </button>
+      </h2>
+      <div
+        id="collapseOne"
+        className="accordion-collapse collapse show"
+        aria-labelledby="headingOne"
+        data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body d-flex flex-wrap gap-3">
+          {status.map((item, index) => (
+            <Filterbtn
+              key={index}
+              index={index}
+              name="status"
+              task={setStatus}
+              setPageNumber={setPageNumber}
+              input={item}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Status;
